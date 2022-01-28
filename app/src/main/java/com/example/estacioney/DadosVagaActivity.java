fe1627ff47a450a041deb6a3e5e87e7da6d50841
Lara Aguilar <72893552+LarAguilar@users.noreply.github.com>
2022-01-28 01:09:12 -0300

    dados vaga sem func

--- app/src/main/java/com/example/estacioney/DadosVagaActivity.java
+++ app/src/main/java/com/example/estacioney/DadosVagaActivity.java
@@ -1,15 +1,23 @@
 package com.example.estacioney;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
+
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class DadosVagaActivity extends AppCompatActivity {
     String idAlocado;
@@ -46,6 +54,38 @@ public class DadosVagaActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // liberar vaga
+                ExecutorService executorService = Executors.newSingleThreadExecutor();
+                executorService.execute(() -> {
+                    HttpRequest httpRequest = new HttpRequest(Config.SERVER_URL_BASE + "liberar.php", "POST", "UTF-8");
+                    httpRequest.setBasicAuth(login, password);
+
+                    httpRequest.addParam("idAlocado", idAlocado);
+
+                    try {
+                        InputStream is = httpRequest.execute();
+                        String result = Util.inputStream2String(is, "UTF-8");
+                        httpRequest.finish();
+
+                        Log.d("HTTP_REQUEST_RESULT", result);
+
+                        JSONObject jsonObject = new JSONObject(result);
+                        final int success = jsonObject.getInt("success");
+                        if(success == 1) {
+                            runOnUiThread(() -> {
+                                // codigo
+                                Intent i = new Intent(DadosVagaActivity.this, EstacActivity.class);
+                                startActivity(i);
+                                finish();
+                            });
+                        } else {
+                            final String error = jsonObject.getString("error");
+                            runOnUiThread(() -> Toast.makeText(DadosVagaActivity.this, error, Toast.LENGTH_LONG).show());
+                        }
+                    } catch (IOException | JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                });
             }
         });
 
